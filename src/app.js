227ade09a90ac51c778c7effc2f0a92e0cee5e9e
Demo <psingla3@atlassian.com>
2026-09-16 18:00:20 +0530

    Add request correlation middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const requestContext = require('./middleware/requestContext');
 const health = require('./routes/health');
 const items = require('./routes/items');
 
 const app = express();
 app.use(express.json());
+app.use(requestContext);
 app.use(health);
 app.use('/api', items);
 
